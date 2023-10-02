9e41c86e2bd73c8e59c1ffab9f9a9a6da5f54004
Alex Purfield <alexpurfield@gmail.com>
2023-10-02 01:26:28 -0600

    it's been 84 years and the new notes are still not posting to the page.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const dataBase = require('./db/db.json');
+const dataBase = './db/db.json';
 const PORT = 3001;
 
 //make app with value of express object methods
@@ -10,6 +10,7 @@ const app = express();
 
 //make static files in public directory available for supply 
 app.use(express.static('public'));
+
 
 
 //3001 port with no routes will go to landing page 
@@ -26,9 +27,6 @@ app.get('/notes', (req, res) => {
 })
 
 //default route 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public' , 'index.html'));
-})
 
 
 //API ROUTES 
@@ -41,27 +39,49 @@ app.get('/api/notes', (req, res) => {
 
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' , 'index.html'));
+})
+
 //POST
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true
 }));
+
 app.post("/api/notes" , (req,res) => { 
-console.info(`${req.method} note received`);
-let response;
-if(req.body.text && req.body.title)   {
-    response = {
-        status: 'success',
-        data:req.body,
-    };
-    res.json(`Note added`);
-} else {
+    
+    let response;
+    if(req.body.text && req.body.title)   {
+        response = {
+            status: 'success',
+            data:req.body,
+        };
+        res.json(`Note added`);
+    } else {
     res.json(`note must have a title and text`);
 }
-const newNote = req.body;
+let newNote = req.body;
+
 console.log(newNote);
 });
+
+app.delete("/api/notes/:id", (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+    //filter all notes that does not have matching id and saved them as a new array
+    //the matching array will be deleted
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
+    })
+
+    //write the updated data to db.json and display the updated note
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
 
 
 //turn on server
