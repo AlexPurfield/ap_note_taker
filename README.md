8c6329d128ef63a31e348097b69f5f7db75fa954
Alex Purfield <alexpurfield@gmail.com>
2023-10-02 19:51:56 -0600

    fixed post and finished project

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# ap_note_taker
+# Express Note Taker
+
+  ## Desctription
+  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+  This is a note taking application that was made using the Express Node web framework. The functionality of this page should allow a user to write and save notes as well as delete them. These notes will be saved and retrieved from a JSON file. 
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Built With](#BuiltWith)
+  * [Screenshots](#Screenshots)
+  * [Questions](#Questions)
+
+  ## Installation
+  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+  Clone Project in GitHub, Run npm i to retrieve all necessary packages
+
+  ## Usage
+  Provide instructions and examples for use. Include screenshots as needed.
+
+  After packages are installed, run node server.js in the terminal or npx nodemon server.js to run the server and the page will be found on localhost:3001
+
+  ## Built With
+  * HTML
+  * CSS
+  * JavaScript
+  * Node.js
+  * Express
+  * uuid
+
+  ## Screenshots
+![Alt text](./images/image.png)
+
+![Alt text](./images/image-1.png)
+
+  ## Questions
+
+  If you have any questions about this application, please reach out to me via email at alexpurfield@gmail.com. You can view my GitHub at https://github.com/alexpurfield
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,90 +1,84 @@
-
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const dataBase = './db/db.json';
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const dataBase = "./db/db.json";
 const PORT = 3001;
 
 //make app with value of express object methods
 const app = express();
+const { v4: uuidv4 } = require("uuid");
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
-//make static files in public directory available for supply 
-app.use(express.static('public'));
+//make static files in public directory available for supply
+app.use(express.static("public"));
 
-
-
-//3001 port with no routes will go to landing page 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public' , 'index.html') );
+//3001 port with no routes will go to landing page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 //HTML ROUTES
 // http://localhost:3001/notes
-//route to notes.html 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-})
-
-//default route 
+//route to notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 
 
-//API ROUTES 
-app.get('/api/notes', (req, res) => {
+//API ROUTES
+app.get("/api/notes", (req, res) => {
     fs.readFile(dataBase, (err, data) => {
         if (err) throw err;
         let dbData = JSON.parse(data);
-        res.json(dbData)
+        res.json(dbData);
     });
+});
 
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public' , 'index.html'));
-})
+//default route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 //POST
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/api/notes" , (req,res) => { 
-    
-    let response;
-    if(req.body.text && req.body.title)   {
-        response = {
-            status: 'success',
-            data:req.body,
-        };
-        res.json(`Note added`);
-    } else {
-    res.json(`note must have a title and text`);
-}
-let newNote = req.body;
-
-console.log(newNote);
+app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // creating unique id for each note
+      id: uuidv4(),
+    };
+    // pushing created note to be written in the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteId = req.params.id.toString();
 
-    //filter all notes that does not have matching id and saved them as a new array
-    //the matching array will be deleted
-    noteList = noteList.filter(selected =>{
-        return selected.id != noteId;
-    })
+  //filter all notes that does not have matching id and saved them as a new array
+  //the matching array will be deleted
+  noteList = noteList.filter((selected) => {
+    return selected.id != noteId;
+  });
 
-    //write the updated data to db.json and display the updated note
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
+  //write the updated data to db.json and display the updated note
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
 });
-
-
 
 //turn on server
 app.listen(PORT, () => {
-    console.log(`listening at http://localhost:${PORT}`);
+  console.log(`listening at http://localhost:${PORT}`);
 });
+
